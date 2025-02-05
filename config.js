20222ceb0a128450b9e2e91d80f64ad3c96cfbc7
whyhussain07 <haha010403@gmail.com>
2025-02-05 07:58:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = [
         usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzM4ODg0Nzg2ZjMwNzQ4NGZiMGQ3NjAiLCJwdWJsaWNBZGRyZXNzIjoiNWt3SkxzZkVaY1oxbWk2NDFaZ0NEcWJvbkJURG9mYmF1MlU3QXplZUZMQWciLCJ3YWxsZXRUeXBlIjoic29sYW5hIiwiaWF0IjoxNzM2Njk4OTg1LCJleHAiOjE3NjgyNTY1ODV9.6RhNpeQtCMrWiw-ajGZgsd9peJ39HyI535eSVbSS9Vo',
         nodes: [
             { nodeId: '12D3KooWLoEvHUrWKgNfQbtkQqChThitNUGA1w74As8SVtFiQSta', hardwareId: 'c5b73c5a2f73cd38928b3a993be64334cf0023228279d871c493aae444b7c103', proxy: 'http://0fdd62698ee81cc8:RNW78Fm5@res.proxy-sale.com:10000' },
-            { nodeId: '12D3KooWMmxwAtPhmcBGYxoN3KakVhJYmDYz6wgicFktptD7enia', hardwareId: '42e739effc26891d14ed5265b44328a0956a1ffd08a551d0c03f3a5d757a7356', proxy: 'http://0fdd62698ee81cc8:RNW78Fm5@res.proxy-sale.com:10001' },
-            { nodeId: '12D3KooWLU7Ejav1wfSqUNdG4NbzxEygU76t7k2pdSpG6ZGNSJ9r', hardwareId: '432e64c2f12750a7e8610bd0abb689500b45dd23a53ea818f6edc48f5aa9a4ca', proxy: 'http://0fdd62698ee81cc8:RNW78Fm5@res.proxy-sale.com:10002' },
+            { nodeId: '12D3KooWMmxwAtPhmcBGYxoN3KakVhJYmDYz6wgicFktptD7enia', hardwareId: '42e739effc26891d14ed5265b44328a0956a1ffd08a551d0c03f3a5d757a7356', proxy: 'http://0fdd62698ee81cc8:RNW78Fm5@res.proxy-sale.com:10042' },
+            { nodeId: '12D3KooWLU7Ejav1wfSqUNdG4NbzxEygU76t7k2pdSpG6ZGNSJ9r', hardwareId: '432e64c2f12750a7e8610bd0abb689500b45dd23a53ea818f6edc48f5aa9a4ca', proxy: 'http://0fdd62698ee81cc8:RNW78Fm5@res.proxy-sale.com:10042' },
             { nodeId: '12D3KooWCvKj7hw43keSWducdxZMzUgDb7Ehxkjtqq28a8uGrDDr', hardwareId: '5e50d5fc5c19910f49b4aee5c9f75f274061a1e675c2a6e324e9ca50b5f35de8', proxy: 'http://0fdd62698ee81cc8:RNW78Fm5@res.proxy-sale.com:10003' },
             { nodeId: '12D3KooWNn2k1yBxo8XuJch1BdECPrZBwpQsAAhaZCg2sYKbMM2j', hardwareId: 'cfd65c5a9a47905dea9bab9d0818a7afd5dfa614a6af7cc8924587601d86bd24', proxy: 'http://0fdd62698ee81cc8:RNW78Fm5@res.proxy-sale.com:10004' }
         ]
